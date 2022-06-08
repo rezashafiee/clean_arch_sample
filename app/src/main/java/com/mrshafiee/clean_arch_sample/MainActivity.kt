fd69d54349f43ef27817f11179fdf15687238ce2
Mohammadreza Shafiee <mrshafiee@hotmail.com>
2022-06-08 22:58:45 +0200

    initial commit

---
+++ app/src/main/java/com/mrshafiee/clean_arch_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mrshafiee.clean_arch_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
